9214277233b89dde943a236eafee71c1269665b6
robo4288 <robo4288@0a4183e2-4043-0410-8c03-cd5d52198443>
2008-01-09 06:52:55 +0000

    Add stub for MockJabberClient::Message

---
+++ t/lib/MockJabberClient/Message.pm
@@ -0,0 +1,27 @@
+package MockJabberClient::Message;
+
+use strict;
+use warnings;
+use Class::Std;
+
+sub GetType {
+    
+}
+
+sub GetFrom {
+    
+}
+
+sub GetSubject {
+    
+}
+
+sub GetBody {
+    
+}
+
+sub GetTimeStamp {
+    
+}
+
+1;
